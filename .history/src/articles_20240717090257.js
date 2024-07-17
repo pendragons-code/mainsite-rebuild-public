925a0c3b314a55e76b14405f3466efd452621bc7
Pen Pen Arthur <pendragonscode@gmail.com>
2024-07-17 11:36:39 +0800

    wee

--- .history/src/articles_20240717090257.js
+++
@@ -1,8 +0,0 @@
-const { readdir } = require("fs");
-async function refreshArticleCache() {
-	readdir("./public/picoctf", async (error, files) => {
-		if(error) return logError(error)
-	});
-}
-refreshArticleCache();
-setInterval(refreshArticleCache, 3.6e+6);
